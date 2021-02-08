39bb618a8921440032b192f921ac374e1c45d2ef
Connor Walden <connorwalden1403@gmail.com>
2021-02-08 18:37:38 +1030

    Changed hero background to a linear gradient going to the right, and changed the direction that the content animates in from

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var timeline = gsap.timeline();
 
 // Everything below the navbar
 timeline.from('.content', {
-  y: '-40%',
+  x: -50,
   opacity: 0,
   duration: 2,
   ease: Power4.easeOut
@@ -15,7 +15,7 @@ timeline.from('.content', {
 // These elements want to pop in slightly offset
 timeline.from('.stagger1', {
   opacity: 0,
-  y: -50,
+  y: 50,
   stagger: .3,
   ease: Power4.easeOut,
   duration: 2
@@ -24,7 +24,7 @@ timeline.from('.stagger1', {
 // Animates the svg I created in Adobe XD 
 timeline.from('.hero-design', {
   opacity: 0,
-  y: -50,
+  y: 50,
   ease: Power4.easeOut,
   duration: 1
 }, "-=2");
